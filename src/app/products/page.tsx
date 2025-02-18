b6f24ba708df937e33ffb4f2a9908a742a10ebc0
Leandro Farias <leandrorf1606@gmail.com>
2025-02-18 17:32:32 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,10 +3,10 @@ import { Input } from "@/components/ui/input";
 
 export default function ProductsPage() {
   return (
-    <div className="p-5 border border-red-500 rounded-xl">
+    <div className="rounded-xl border border-red-500 p-5">
       <h1 className="text-red-500">Pagina de Produtos</h1>
       <Button>Clicar</Button>
       <Input placeholder="Comecando a usar o uishad" />
     </div>
-  )
+  );
 }
